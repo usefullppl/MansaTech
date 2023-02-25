0d432cf5d5e8a57b920a0748d61c8a56e28c83ae
usefullppl <126338022+usefullppl@users.noreply.github.com>
2023-02-26 00:06:05 +0700

    Update RELAY_EX.ino

--- RELAY_EX.ino
+++ RELAY_EX.ino
@@ -1,24 +1,14 @@
 #define r1 8
-int Trig=4;
-int Echo=5;
-int LED=6;
 void setup() {
   pinMode(r1, OUTPUT);
-  pinMode(Trig, OUTPUT);  
-  pinMode(Echo, INPUT);
-  pinMode(LED, OUTPUT);
 }
-
 void loop() {
-  digitalWrite(Trig, HIGH); // Ini menembakkan ultrasound.
-      delayMicroseconds(10); //Ultrasonografi dihasilkan hanya dalam 10 mikrodetik
-  digitalWrite(Trig, LOW); // Hentikan USG.
-  
-  int cm = pulseIn(Echo, HIGH)/58; // Ubah waktu yang diukur dgn ultrasound menjadi cm
-  
-  if(cm<5){ // Saat rintangan masuk dalam jarak 20cm, alarm berbunyi.
   digitalWrite(r1,LOW); 
-  }else{
+  delay(1000);
   digitalWrite(r1,HIGH);
-  }
+  delay(1000);
+  digitalWrite(r1,LOW); 
+  delay(1000);
+  digitalWrite(r1,HIGH);
+  delay(1000);
 }
